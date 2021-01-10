fe51dac8c41b9e328bea5aaba6661bf4d8094092
Raghavender <raghukatrapally689@gmail.com>
2021-01-10 21:20:13 +0530

    The files are added to git

---
+++ src/main/java/Raghu/king/App.java
@@ -0,0 +1,13 @@
+package Raghu.king;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Raghu/king/Arraylista.java
@@ -0,0 +1,20 @@
+package Raghu.king;
+
+import java.util.ArrayList;
+
+public class Arraylista {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	ArrayList<String> arr=new ArrayList<String>() ;
+	arr.add("Raghu");
+	arr.add("King");
+	
+	System.out.println("The names are" +arr);
+	
+		
+	
+
+	}
+
+}
